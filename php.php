11a79e6e5e6aadebfcee11e98762ee457e6ce46a
Wagner Braga <wfbraga@hotmail.com>
2020-06-01 12:36:28 -0300

    First Commit

---
+++ php.php
@@ -0,0 +1,9 @@
+<?php
+    $fname = (isset($_POST['fname'])) ? $_POST['fname']: 'No name given';
+    $lname = (isset($_POST['lname'])) ? $_POST['lname']: 'No name given';
+    $somevalue = "whatever";
+
+    $array = ['fname'=>$fname, 'lname'=>$lname, 'response'=> $somevalue];
+
+    echo json_encode($array);
+    
---
+++ test.php
@@ -0,0 +1,2 @@
+<?php
+    echo '<h1> Usando php</h1>';
